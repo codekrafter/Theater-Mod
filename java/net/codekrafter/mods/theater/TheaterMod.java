e2c10725dff435d13fb5dcebc377c15151d9aebc
codekrafter <codekrafter@gmail.com>
2015-06-06 08:58:05 -0400

    Worked On I/O For Data Folder

--- java/net/codekrafter/mods/theater/TheaterMod.java
+++ java/net/codekrafter/mods/theater/TheaterMod.java
@@ -1,5 +1,10 @@
 package net.codekrafter.mods.theater;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import net.codekrafter.mods.theater.init.TheaterBlocks;
 import net.codekrafter.mods.theater.init.TheaterItems;
 import net.codekrafter.mods.theater.proxy.CommonProxy;
@@ -11,6 +16,8 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 
+import org.yaml.snakeyaml.Yaml;
+
 @Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
 public class TheaterMod
 {
@@ -18,6 +25,22 @@ public class TheaterMod
     @SidedProxy(clientSide = Reference.CLIENT_PROXY_CLASS, serverSide = Reference.SERVER_PROXY_CLASS)
     public static CommonProxy proxy;
     public static final CreativeTabs stage_tab = new TheaterTab("theater_tab");
+    Yaml yaml = new Yaml();
+    File baseDir = new File("Theater Mod");
+    File config = new File(baseDir, "config.yml");
+    FileOutputStream configOut;
+    FileInputStream configIn;
+    {
+	try
+	{
+	    configOut = new FileOutputStream(config);
+	} catch (IOException e)
+	{
+	    System.err
+		    .println("Error Creating Output Stream For Config (\"config.yml\") Stack Trace Incoming:");
+	    e.printStackTrace();
+	}
+    }
 
     @EventHandler
     public void preInit(FMLPreInitializationEvent event)
@@ -26,13 +49,29 @@ public class TheaterMod
 	TheaterBlocks.register();
 	TheaterItems.init();
 	TheaterItems.register();
+	if (!baseDir.exists())
+	{
+	    baseDir.mkdir();
+	}
+	if (!config.exists())
+	{
+	    try
+	    {
+		config.createNewFile();
+	    } catch (IOException e)
+	    {
+		e.printStackTrace();
+	    }
+	}
+
     }
 
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
 	proxy.registerRenders();
-	System.out.println("CURRENT VERSIONS EASTER EGG:" + " " + Reference.EASTER_EGG);
+	System.out.println("ggE Retsae Tnerruc Eht:" + " "
+		+ Reference.EASTER_EGG);
     }
 
     @EventHandler
